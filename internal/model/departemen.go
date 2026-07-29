05960d1e9a76c8d109bafe7410a00c63d8730935
seymourrisey <gusfathon.fm@gmail.com>
2026-07-29 17:11:23 +0700

    fix: model departemen struct

--- internal/model/departemen.go
+++ internal/model/departemen.go
@@ -8,6 +8,6 @@ import "time"
 // soft-delete)
 type Departemen struct {
 	ID        int       `json:"id" db:"departemen_id"`
-	Nama      string    `json:"nama" db:"nama"`
+	Nama      string    `json:"nama" db:"nama_departemen"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
